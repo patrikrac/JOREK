54276ae52743e0bdccba255e7a24fa80ecb7550e
Patrik Rác <patrik.rac@ipp.mpg.de>
2025-03-25 14:55:45 +0100

    Removing save of PC blocks from solverr

--- solvers/mod_sparse.f90
+++ solvers/mod_sparse.f90
@@ -56,13 +56,13 @@ module mod_sparse
     sol_vec%n = rhs_vec%n
 
     verbose = solver%verbose.and.(my_id.eq.0)
-#ifdef SAVEMATRIX
-    write(fname,'(A5,I2.2,A3)') "matA_",my_id,".h5"
-    call save_mat_h5_ext(fname, a_mat%ng, a_mat%ng, a_mat%nnz, &
-                           a_mat%irn, a_mat%jcn, a_mat%val, rhs=rhs_vec%val, &
-                           ind_min=a_mat%index_min(my_id+1),ind_max=a_mat%index_max(my_id+1), &
-                           block_size=a_mat%block_size)
-#endif
+! #ifdef SAVEMATRIX
+!     write(fname,'(A5,I2.2,A3)') "matA_",my_id,".h5"
+!     call save_mat_h5_ext(fname, a_mat%ng, a_mat%ng, a_mat%nnz, &
+!                            a_mat%irn, a_mat%jcn, a_mat%val, rhs=rhs_vec%val, &
+!                            ind_min=a_mat%index_min(my_id+1),ind_max=a_mat%index_max(my_id+1), &
+!                            block_size=a_mat%block_size)
+! #endif
 
     if (.not.solver%iterative) then
 
@@ -137,14 +137,14 @@ module mod_sparse
       endif
 
       call update_pc_rhs(solver%pc,rhs_vec)
-#ifdef SAVEMATRIX
-      if (.not.solver%solve_only) then
-        write(fname,'(A3,I2.2,A3)') "pc_",my_id,".h5"
-        call save_mat_h5_ext(fname, a_mat%ng, solver%pc%mat%ng,solver%pc%mat%nnz, &
-                                       solver%pc%mat%irn, solver%pc%mat%jcn,solver%pc%mat%val, &
-                                       l2g=solver%pc%row_index,rhs=solver%pc%rhs%val, block_size=solver%pc%mat%block_size)
-      endif
-#endif
+! #ifdef SAVEMATRIX
+!       if (.not.solver%solve_only) then
+!         write(fname,'(A3,I2.2,A3)') "pc_",my_id,".h5"
+!         call save_mat_h5_ext(fname, a_mat%ng, solver%pc%mat%ng,solver%pc%mat%nnz, &
+!                                        solver%pc%mat%irn, solver%pc%mat%jcn,solver%pc%mat%val, &
+!                                        l2g=solver%pc%row_index,rhs=solver%pc%rhs%val, block_size=solver%pc%mat%block_size)
+!       endif
+! #endif
       if (solver%library.eq.mumps) then
 #ifdef USE_MUMPS
         call solve_mumps_all(solver%mmss, solver%pc%mat, solver%pc%rhs, solver%solve_only, tag)
